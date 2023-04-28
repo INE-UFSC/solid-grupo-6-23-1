a49914f01f4367cfba4e7c9b9264fa44b0d82838
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2023-04-28 14:27:26 -0300

    fix: dip

--- 5.dip.py
+++ 5.dip.py
@@ -4,10 +4,11 @@ Dependency Inversion Principle
 Dependências devem ser feitas sobre abstrações, não sobre implementações concretas 
 
 """
-
+from abc import ABC, abstractmethod
 
 class Player:
     def __init__(self, name):
+        self.stats = PrintPlayerReport(self)
         self.__name = name
         self.__hp = 100
         self.__speed = 1
@@ -17,8 +18,24 @@ class Player:
 
     def name(self):
         return self.__name
+    
+    def speed(self):
+        return self.__speed
+
+class IPlayerReporter(ABC):
+    @abstractmethod
+    def __init__(self, player: Player):
+        pass
+
+    @abstractmethod
+    def report(self):
+        pass
+
+class PrintPlayerReport(IPlayerReporter):
+    def __init__(self, player: Player):
+        self.__player = player
 
     def report(self):
-        print(f'Name:{self.__name()}')
-        print(f'HP:{self.__hp()}')
-        print(f'Speed:{self.__speed()}')
+        print(f'Name:{self.__player.name()}')
+        print(f'HP:{self.__player.hp()}')
+        print(f'Speed:{self.__player.speed()}')
